4b4b5ea8813b09f5aefe3c26b7c4cc94c626cc87
jack ayaga <jacksonochieng@hmail.com>
2025-07-06 17:45:52 +0300

    renamed the signup.html from sign_up to signup

--- monitor/urls.py
+++ monitor/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('signup/', views.signup_view, name='signup'),  # ✅ NEW
+    path('signup/', views.signup_view, name='signup'),  
     path('export/pdf/', views.export_pdf, name='export_pdf'),
     path('chart/temperature/', views.temperature_chart, name='temperature_chart'),
     path('api/animals/', views.api_animals),
